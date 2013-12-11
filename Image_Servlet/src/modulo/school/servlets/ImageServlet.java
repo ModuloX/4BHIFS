eb33fe19ae88c9b9b52457d44269e2a19120ac09
ModuloX <redlord307@hotmail.com>
2013-12-11 20:39:33 +0100

    added Image Servlet

---
+++ Image_Servlet/src/modulo/school/servlets/ImageServlet.java
@@ -0,0 +1,51 @@
+package modulo.school.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.OutputStream;
+import javax.imageio.ImageIO;
+
+/**
+ * Servlet implementation class ImageServlet
+ */
+@WebServlet("/ImageServlet")
+public class ImageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ImageServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("image/jpeg");
+
+		String pathToWeb = getServletContext().getRealPath(File.separator);
+		File f = new File(pathToWeb + "ModuloX.jpg");
+		BufferedImage bi = ImageIO.read(f);
+		OutputStream out = response.getOutputStream();
+		ImageIO.write(bi, "jpg", out);
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
